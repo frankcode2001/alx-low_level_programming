d6e73420efdc08826e93abca2eea31c315b5dc18
root <root@frankcode.localdomain>
2022-03-10 12:10:43 +0300

    use the printf function

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -6,7 +6,8 @@
 *
 * return (0) = Success
 */
+int main(void)
 {
-	printf("\"with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
