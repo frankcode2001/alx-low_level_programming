c9b0b023e32faf364315bd24656b8ed60b3fe829
frankcode2001 <97832998+frankcode2001@users.noreply.github.com>
2022-03-21 10:57:31 +0300

    Update 7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * puts_half - a function that prints the second half of a string followed by a new line
+ * puts_half - prints the second half of a string
  * @str: string to print
  *
  * Return: void
